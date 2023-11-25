5435d0ce92de96fc78237996eff3026f8460d706
SIERGH <131610152+SIERGH@users.noreply.github.com>
2023-11-25 20:18:28 -0300

    projeto de sistema ranqueado da DIO

---
+++ main.js
@@ -0,0 +1,38 @@
+function classificarHeroi(nomeHeroi, xpHeroi) {
+
+    let nivel;
+
+    switch (true) {
+        case xpHeroi < 1000:
+            nivel = "Ferro";
+            break;
+        case xpHeroi >= 1001 && xpHeroi <= 2000:
+            nivel = "Bronze";
+            break;
+        case xpHeroi >= 2001 && xpHeroi <= 5000:
+            nivel = "Prata";
+            break;
+        case xpHeroi >= 6001 && xpHeroi <= 7000:
+            nivel = "Ouro";
+            break;
+        case xpHeroi >= 7001 && xpHeroi <= 8000:
+            nivel = "Platina";
+            break;
+        case xpHeroi >= 8001 && xpHeroi <= 9000:
+            nivel = "Ascendente";
+            break;
+        case xpHeroi >= 9001 && xpHeroi <= 10000:
+            nivel = "Imortal";
+            break;
+        default:
+            nivel = "Radiante";
+            break;
+    }
+
+    console.log(`${nomeHeroi} atingiu o nível ${nivel} com ${xpHeroi} de XP.`);
+}
+
+const nomeHeroi = "Saci"
+const xpHeroi = 11000
+
+classificarHeroi(nomeHeroi,xpHeroi);
